4e85ac0e3e81015549425dcb3f97b32b35398b4d
Eugene Myroniuk <eugene.my88@gmail.com>
2017-05-18 09:47:09 -0700

    First input

---
+++ login.js
@@ -0,0 +1,24 @@
+// Initialize Firebase
+initFirebaseApp();
+
+// Login state control. If logged in, redirect. if not, stay on page.
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser) {
+    //TODO get redirect results, pull the FB list
+    window.location = "main.html";
+  } else {
+    bodyDisplay.classList.remove('hide');
+  }
+});
+
+// call this function on login onclick button
+function signInFlow() {
+  var provider = new firebase.auth.FacebookAuthProvider();
+  provider.addScope('user_friends');
+  provider.addScope('email');
+  provider.setCustomParameters({
+    'display': 'popup'
+  });
+  firebase.auth().signInWithRedirect(provider);
+}
+w
