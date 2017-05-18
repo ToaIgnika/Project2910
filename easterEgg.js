6a74993c8fc8602cd2ae23f24803e85fd50fac14
Alex <alexjcampbe@gmail.com>
2017-05-18 09:17:33 -0700

    starts of easter egg

---
+++ easterEgg.js
@@ -0,0 +1,14 @@
+function easter(){
+	var secret = "3838404037393739666513" // konami code
+	var input = "";
+	var timer;
+	var mode = false;
+	var show = false;
+	
+}
+function check_input {
+	if(input == secret){
+		show = true;
+	}
+}
+
