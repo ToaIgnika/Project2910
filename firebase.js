83256abc9a6f44bacf98c3f12144893b9dd4cc94
mdumycz <m.dumycz.business@gmail.com>
2017-05-18 12:23:50 -0700

    Added User Function
    baseline user function with debug comments

--- firebase.js
+++ firebase.js
@@ -11,3 +11,41 @@ function initFirebaseApp() {
   };
   firebase.initializeApp(config);
 }
+
+// node getter. returns as an reference object.
+function getNodeAt(nLocation) {
+  return firebase.database().ref().child(nLocation);
+}
+
+// displays user data from database from a given user ID
+function displayUserInfo(user) {
+	const profile = getNodeAt('users/' + user);
+	
+	// for debug/testing
+	window.alert(user + " | " + profile);
+	
+	profile.once("value")
+		.then(function(snapshot) {
+			
+			// for debug/testing
+			window.alert(snapshot.key);
+			
+			snapshot.forEach(function(field) {
+				var key = field.key;
+				var val = field.val();
+				
+				// for debug/testing
+				window.alert(key + " : " + field.val());
+				
+				// put the output somewhere useful here
+				var insert = document.getElementById(key);
+				if (insert != null) {
+					if (insert.tagName == "img") {
+						insert.setAttribute("src", val);
+					} else {
+						insert.innerText = val;
+					}
+				}
+			});	
+		});	
+}
