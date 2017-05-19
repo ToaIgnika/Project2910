c987bcd887c815c4bbb17121c559cde8bb4a4ff1
remytruong <remtruong@live.ca>
2017-05-19 10:55:56 -0700

    Reorganization

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ initFirebaseApp();
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if(firebaseUser) {
     //TODO get redirect results, pull the FB list
-    window.location = "home.html";
+    window.location = "../home.html";
   } else {
     const wrap = document.getElementById('body_wrap');
     wrap.classList.remove('hide');
