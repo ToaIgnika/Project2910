c6212cf53aa438b627957d0f8f5ecc756cb42639
Eugene Myroniuk <eugene.my88@gmail.com>
2017-05-24 00:26:51 -0700

    Chat functionality part 1
    Added chat page and JS fuctionality to load chats

---
+++ js/chat.js
@@ -0,0 +1,127 @@
+// Initialize Firebase
+initFirebaseApp();
+
+// get elemets from the html dom
+//TODO
+
+// login status checker
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser) {
+    // TODO control the DB interaction functions
+    displayText();
+    const wrap = document.getElementById('body_wrap');
+    wrap.classList.remove('hide');
+  } else {
+    window.location = "index.html";
+  }
+});
+
+
+function displayText() {
+  var user = firebase.auth().currentUser.uid;
+  const chatList = getNodeAt('/chat/');
+  chatList.off();
+  const chatWindow = document.getElementById('chatWindowHTML');
+
+  chatList.on('child_added', function(snap) {
+      var otherU = snap.key.replace(user, '');
+      if (otherU.length != snap.key) {
+        // create button for collapse
+        const chatButton = document.createElement('button');
+        chatButton.style.width = "100%";
+        chatButton.setAttribute('data-toggle','collapse');
+        chatButton.setAttribute('data-target', '#' + otherU);
+        chatButton.className = 'btn btn-outline-success';
+        chatButton.innerHTML = otherU;
+
+        // create ul/input field block
+        const chatBlock = document.createElement('div');
+        chatBlock.classList.add('collapse');
+        chatBlock.id = otherU;
+
+        // create ul window
+        const chatUl = document.createElement('ul');
+        chatUl.classList.add('ulChatText');
+
+        // create input field
+        const chatInputField = document.createElement('input');
+        chatInputField.setAttribute('type', 'text');
+        chatInputField.setAttribute('onchange','sendText(this.id)');
+        chatInputField.classList.add('form-control');
+        chatInputField.id = snap.key;
+
+
+        chatWindow.appendChild(chatButton);
+        chatWindow.appendChild(chatBlock);
+        chatBlock.appendChild(chatUl);
+        chatBlock.appendChild(chatInputField);
+
+        const chatNode = getNodeAt('/chat/' + snap.key);
+        chatNode.on.limitToLast(10).('child_added', function(snapT) {
+          const li = document.createElement('li');
+          if (snapT.val().user == user) {
+            li.classList.add('myLi');
+          } if (snapT.va;().user == "system") {
+            li.classList.add("systemLi")
+          } else {
+            li.classList.add('otherLi');
+          }
+          li.innerHTML = snapT.val().msg;
+          chatUl.appendChild(li);
+        });
+      }
+    });
+}
+
+function sendText(textItemVal) {
+  console.log(textItemVal);
+  var user = firebase.auth().currentUser;
+  var textVal = document.getElementById(textItemVal).value;
+  console.log(user.uid + document.getElementById(textItemVal).value);
+  if (user && textVal != '') {
+    console.log('called twice');
+    const chatTxt = getNodeAt('chat/' + textItemVal).push().set({
+      'msg' : textVal,
+      'user' : user.uid
+    });
+  document.getElementById(textItemVal).value = '';
+  } else {
+    //textMsg.value = 'you are not logged in. Please, reload the page!';
+  }
+}
+
+function goToChat(posterUidVal) {
+  var user = firebase.auth().currentUser.uid;
+  var chatCatch = uidBond(posterUidVal.value, user);
+  if (getNodeAt('/chat/').hasChild(chatCatch)) {
+    //TODO redirect to chat with posttemp message and chat opened (ffs)
+    getNodeAt('/chat/' + chatCatch).push().set({
+      'msg' : 'Hi, sup with this item?',
+      'user' : user
+    });
+  } else {
+    getNodeAt('/chat/' + chatCatch).push().set({
+      'msg' : 'Welcome to the chat!',
+      'user' : 'system'
+    });
+    //TODO create chat w/ welcome message and redirect there
+  }
+
+  // create button to delete item
+  function textButton (place, btnVal) {
+    var btn = document.createElement("BUTTON");        // Create a <button> element
+    //btn.setAttribute("id", "btnRemove");
+    btn.setAttribute("value", btnVal);
+    btn.setAttribute("onclick", "goToChat(this)");
+    var s = document.createElement("input");
+    s.src = "images/del_icon.png";
+    s.type = "image";
+    btn.appendChild(s);
+    //var t = document.createTextNode('<img src ="images/icon.png">');    // Create a text node
+    //btn.appendChild(t);                                // Append the text to <button>
+    place.appendChild(btn);                    // Append <button> to <body>
+  }
+
+  // textButton(newLiPlace, snap.val().poster_id);
+
+}
