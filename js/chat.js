828e10ea45efe31ac81405f93e3a358c1be3dd69
Eugene Myroniuk <eugene.my88@gmail.com>
2017-05-24 12:32:31 -0700

    Chat integration part 3
    workinprogress: added styling and shit

--- js/chat.js
+++ js/chat.js
@@ -25,14 +25,22 @@ function displayText() {
 
   chatList.on('child_added', function(snap) {
       var otherU = snap.key.replace(user, '');
-      if (otherU.length != snap.key) {
+      if (otherU.length != snap.key.length) {
+        // create panel div
+        const divPanel = document.createElement('div');
+        divPanel.className = 'panel';
+
         // create button for collapse
         const chatButton = document.createElement('button');
         chatButton.style.width = "100%";
         chatButton.setAttribute('data-toggle','collapse');
         chatButton.setAttribute('data-target', '#' + otherU);
+        chatButton.setAttribute('data-parent', '#accordion');
         chatButton.className = 'btn btn-outline-success';
-        chatButton.innerHTML = otherU;
+
+        getNodeAt('/users/' + otherU).once('value', function(snip) {
+          chatButton.innerHTML = snip.val().user_name;
+        })
 
         // create ul/input field block
         const chatBlock = document.createElement('div');
@@ -50,21 +58,23 @@ function displayText() {
         chatInputField.classList.add('form-control');
         chatInputField.id = snap.key;
 
-
+        divPanel.appendChild(chatButton);
+        divPanel.appendChild(chatBlock);
         chatWindow.appendChild(chatButton);
         chatWindow.appendChild(chatBlock);
         chatBlock.appendChild(chatUl);
         chatBlock.appendChild(chatInputField);
+        chatWindow.appendChild(divPanel);
 
         const chatNode = getNodeAt('/chat/' + snap.key);
         chatNode.limitToLast(10).on('child_added', function(snapT) {
           const li = document.createElement('li');
           if (snapT.val().user == user) {
-            li.classList.add('myLi');
-          } if (snapT.val().user == "system") {
-            li.classList.add("systemLi")
+            li.className = ('myLi');
+          } else if (snapT.val().user == "system") {
+            li.className = ("systemLi");
           } else {
-            li.classList.add('otherLi');
+            li.className = ('otherLi');
           }
           li.innerHTML = snapT.val().msg;
           chatUl.appendChild(li);
