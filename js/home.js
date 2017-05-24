6e7fbe38e22bdbe1fd11bbf6d871a97fcdd5eeee
remytruong <remtruong@live.ca>
2017-05-24 13:30:23 -0700

    Fixing tab

--- js/home.js
+++ js/home.js
@@ -207,3 +207,9 @@ function uidBond(uidOne, uidTwo) {
     return uidOne + uidTwo;
   }
 }
+
+$('.nav-tabs').on('shown.bs.tab', 'a', function (e) {
+    if (e.relatedTarget) {
+        $(e.relatedTarget).removeClass('active');
+    }
+})
